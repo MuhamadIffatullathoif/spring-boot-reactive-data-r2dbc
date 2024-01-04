a4cc9baa69e277b5b0d9583dfd3e3927d47a1e9a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-04 11:22:34 +0700

    11. Create New Beer Entity

--- src/main/java/com/iffat/springbootreactivedatar2dbc/services/BeerService.java
+++ src/main/java/com/iffat/springbootreactivedatar2dbc/services/BeerService.java
@@ -1,6 +1,7 @@
 package com.iffat.springbootreactivedatar2dbc.services;
 
 import com.iffat.springbootreactivedatar2dbc.model.BeerDTO;
+import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -8,4 +9,6 @@ public interface BeerService {
     Flux<BeerDTO> listBeers();
 
     Mono<BeerDTO> getBeerById(Integer beerId);
+
+    Mono<BeerDTO> saveNewBeer(BeerDTO beerDTO);
 }
