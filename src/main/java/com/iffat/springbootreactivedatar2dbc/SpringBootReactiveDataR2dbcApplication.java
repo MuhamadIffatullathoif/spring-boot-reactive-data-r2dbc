4091e8328d2b8fca5820cad67b703a9cd5b76798
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-04 09:09:53 +0700

    01. Starter

---
+++ src/main/java/com/iffat/springbootreactivedatar2dbc/SpringBootReactiveDataR2dbcApplication.java
@@ -0,0 +1,13 @@
+package com.iffat.springbootreactivedatar2dbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveDataR2dbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactiveDataR2dbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iffat/springbootreactivedatar2dbc/SpringBootReactiveDataR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iffat.springbootreactivedatar2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveDataR2dbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
