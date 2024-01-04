9b8b5de6fbd32d8c846abfcd0c55a816a80ed67a
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-04 09:41:07 +0700

    05. Test Save new Entity

---
+++ src/test/java/com/iffat/springbootreactivedatar2dbc/repositories/BeerRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.iffat.springbootreactivedatar2dbc.repositories;
+
+import com.iffat.springbootreactivedatar2dbc.domain.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataR2dbcTest
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void saveNewBeer() {
+        beerRepository.save(getTestBeer())
+                .subscribe(beer -> {
+                    System.out.println(beer.toString());
+                });
+    }
+
+    Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("Space Dust")
+                .beerStyle("IPA")
+                .price(BigDecimal.TEN)
+                .quantityOnHand(12)
+                .upc("123123")
+                .build();
+    }
+}
