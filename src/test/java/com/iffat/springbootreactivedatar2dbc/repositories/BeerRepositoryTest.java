e2d34b9d149268e8073e1abab5339b1965bc0b4b
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-04 09:48:01 +0700

    06. Add Create and Update Date with Auditing

--- src/test/java/com/iffat/springbootreactivedatar2dbc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/iffat/springbootreactivedatar2dbc/repositories/BeerRepositoryTest.java
@@ -1,15 +1,18 @@
 package com.iffat.springbootreactivedatar2dbc.repositories;
 
+import com.iffat.springbootreactivedatar2dbc.config.DatabaseConfig;
 import com.iffat.springbootreactivedatar2dbc.domain.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
 
     @Autowired
