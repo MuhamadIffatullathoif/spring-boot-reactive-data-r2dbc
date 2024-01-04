b850f89606ddacf615f412b1667a324b6ac9e297
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-04 11:12:52 +0700

    10. Using path variable

--- src/main/java/com/iffat/springbootreactivedatar2dbc/services/BeerService.java
+++ src/main/java/com/iffat/springbootreactivedatar2dbc/services/BeerService.java
@@ -2,7 +2,10 @@ package com.iffat.springbootreactivedatar2dbc.services;
 
 import com.iffat.springbootreactivedatar2dbc.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
     Flux<BeerDTO> listBeers();
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 }
